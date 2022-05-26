15b8572c71a73b6c8afbc895f69893ceedf6c37f
P00LY <alexjmarsh55@gmail.com>
2022-05-25 17:14:22 -0700

    add form functionality and new book generation
    add form submit event listener.
    add book constructor.
    add new book generation and set to local storage.
    add function for book tile element creation.

---
+++ app.js
@@ -0,0 +1,63 @@
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const status = document.getElementById('status');
+const form = document.getElementById('form')
+const tile_container = document.getElementById('tile-container');
+
+// Form submit - adds book to library + local storage
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    clearForm();
+})
+
+
+let myLibrary = [];
+
+class Book {
+    constructor(title, author, status) {
+        this.title = title;
+        this.author = author;
+        this.status = status;
+    }
+}
+
+//ADDS BOOK TO LIBRARY AND LOCAL STORAGE
+function addBookToLibrary() {
+    if (title.value.legnth === 0 || author.value.length === 0) {
+        alert("Please fill all fields");
+        return;
+    }
+    const newBook = new Book(title.value, author.value, status.value);
+    myLibrary.push(newBook);
+    updateLocalStorage();
+}
+
+// Set Item to Local Storage 
+function updateLocalStorage() {
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+}
+
+// CLEAR FORM
+function clearForm() {
+    title.value = "";
+    author.value = "";
+}
+
+//CREATES BOOK TILE
+function createTile(item) {
+    const library = document.getElementById('tile-container');
+    const bookTile = document.createElement('div');
+    const bookTitle = document.createElement('div');
+    const bookAuthor = document.createElement('div');
+    const bookStatus = document.createElement('select');
+    const bookDelete = document.createElement('button');
+
+    bookTile.setAttribute('id', myLibrary.indexOf(item));
+
+    bookTitle.textContent = item.title;
+    bookTile.appendChild(bookTitle);
+
+    bookAuthor.textContent = item.author;
+    bookTile.appendChild(bookAuthor);
+}
