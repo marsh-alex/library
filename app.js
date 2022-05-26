f60fdb3e4e1375d05150734d5d32e0340378a2ac
P00LY <alexjmarsh55@gmail.com>
2022-05-26 14:00:58 -0700

    create elements and render book tile function

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ function addBookToLibrary() {
     const newBook = new Book(title.value, author.value, status.value);
     myLibrary.push(newBook);
     updateLocalStorage();
+    render();
 }
 
 // Set Item to Local Storage 
@@ -51,13 +52,62 @@ function createTile(item) {
     const bookTitle = document.createElement('div');
     const bookAuthor = document.createElement('div');
     const bookStatus = document.createElement('select');
+    const statusOption = document.createElement('option');
     const bookDelete = document.createElement('button');
 
     bookTile.setAttribute('id', myLibrary.indexOf(item));
 
     bookTitle.textContent = item.title;
+    bookTitle.classList.add('titleTile');
     bookTile.appendChild(bookTitle);
 
     bookAuthor.textContent = item.author;
+    bookAuthor.classList.add('authorTile');
     bookTile.appendChild(bookAuthor);
+
+    bookStatus.textContent = item.status;
+    bookStatus.classList.add('statusTile');
+    bookTile.appendChild(bookStatus);
+
+    statusOption.textContent = 'Read';
+    statusOption.value = 'Read';
+    bookStatus.appendChild(statusOption);
+
+    statusOption.textContent = 'Not Read';
+    statusOption.value = "Not Read";
+
+    statusOption.textContent = 'Reading';
+    statusOption.value = "Reading";
+    bookStatus.appendChild(statusOption);
+
+    bookDelete.textContent = 'Delete';
+    bookDelete.classList.add('deleteBtn');
+    bookTile.appendChild(bookDelete);
+
+    library.appendChild(bookTile);
 }
+
+//Creates visual tile in browser
+function render() {
+    const library = document.getElementById('tile-container');
+    const bookTile = document.querySelectorAll('.bookTile');
+    bookTile.forEach(bookTile => library.removeChild(bookTile))
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        createTile(myLibrary[i]);
+    }
+}
+
+//Renders books from local storage after page refresh
+function restore() {
+    if (!localStorage.myLibrary) {
+        render();
+    } else {
+        let objects = localStorage.getItem('myLibrary')
+        objects = JSON.parse(objects);
+        myLibrary = objects;
+        render();
+    }
+}
+
+restore();
